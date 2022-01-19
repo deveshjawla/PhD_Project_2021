77c2fcc96e0b172ecc781ead157f04edc9af948e
Devesh <devesh.jawla@gmail.com>
2022-01-19 08:02:03 +0000

    frequentist_CNN_done

--- Networks.jl
+++ Networks.jl
@@ -1,9 +1,17 @@
-using Flux, Flux.Data.MNIST, Statistics
-using Flux: onehotbatch, onecold, crossentropy, throttle
-using Base.Iterators: repeated, partition
+#### 
+#### Network - A customizable chain of conv layer and dense layers. Does not require manually
+#### calculating the output size of images. Takes as input batches of images with dims WHCN.
+#### 
+
+using Flux, Statistics
 using Printf, BSON
 
-function dense_layer(indim::Int, outdim::Int, bnmom::Float32, activation_fn)
+"""
+# Function to make a custom dense layer
+"""
+function dense_layer(indim::Int, outdim::Int,
+	bnmom::Float32, activation_fn)
+
     if typeof(bnmom) == Float32
         Chain(
             Dense(indim, outdim),
@@ -14,14 +22,38 @@ function dense_layer(indim::Int, outdim::Int, bnmom::Float32, activation_fn)
     end
 end
 
-function conv_layer(input_dims, filter_size::Tuple{Int,Int}, i::Int, o::Int, activation_fn, stride_length, pool_window::Tuple{Int,Int}, bnmom::Float32)
-    pad = size .÷ 2
-    layer = Chain(
-        Conv(filter_size, i => o, activation_fn, pad = pad),
-        x -> meanpool(x, pool_window),
-        BatchNorm(i, activation_fn, momentum = bnmom)
-    )
-	output_dims = (((input_dims[1] - filter_size[1] + 2*pad[1]) / stride_length) + 1)
-    return layer, output_dims
+"""
+# Fucntion to make a custom Covolution layer.
+## Returns a tuple, (layer, output_image_dims)
+"""
+function conv_layer(input_dims, filter_size::Tuple{Int,Int}, 
+	i::Int, o::Int, activation_fn, pool_window::Tuple{Int,Int}; 
+	stride_length=1, pool_stride=1, bnmom=0.5)
+
+    pad = filter_size .÷ 2
+	if typeof(bnmom) == Float32
+		layer = Chain(
+			Conv(filter_size, i => o, activation_fn, pad = pad),
+			x -> meanpool(x, pool_window),
+			BatchNorm(i, activation_fn, momentum = bnmom)
+		)
+	else
+		layer = Chain(
+			Conv(filter_size, i => o, activation_fn, pad = pad),
+			x -> meanpool(x, pool_window, stride= pool_stride)
+			)
+	end
+    output_dims_conv = (((input_dims[1] - filter_size[1] + 2 * pad[1]) / stride_length) + 1)
+	output_dims = ((output_dims_conv - pool_window[1])/pool_stride) + 1
+    return layer, Int(output_dims)
 end
 
+conv_1, out_1 = conv_layer(128, (3,3), 1, 1, relu, (2,2), bnmom=nothing)
+conv_2, out_2 = conv_layer(out_1, (3,3), 1, 1, relu, (2,2), bnmom=nothing)
+conv_3, out_3 = conv_layer(out_2, (3,3), 1, 1, relu, (2,2), bnmom=nothing)
+dense_1 = dense_layer(out_3^2, 128, 0.1f0, relu)
+dense_2 = dense_layer(128, 10, 0.1f0, relu)
+
+# An example Network. You may customize each layer above or remove/add layers as needed.
+network = Chain(conv_1, conv_2, conv_3, flatten, dense_1, dense_2, softmax)
+# network(rand(128,128,1,1))
